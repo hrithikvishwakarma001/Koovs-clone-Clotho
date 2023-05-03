22df415260b2543908612f270d039dad75830e45
Ga1156 <gauravpardeshi068@gmail.com>
2023-05-03 14:53:37 +0530

    folder_structure

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
+import { BrowserRouter } from "react-router-dom"
+import { ChakraProvider } from "@chakra-ui/react"
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ChakraProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ChakraProvider>
+
+
+
 )
