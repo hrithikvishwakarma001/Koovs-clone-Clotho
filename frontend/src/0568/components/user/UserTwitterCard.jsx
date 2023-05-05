6024f2705bdf4d8e1482157fb71f479e8c9520d3
Hrithik Vishwakarma <hritik8008@gmail.com>
2023-05-05 17:37:40 +0530

    admin ui frontend

---
+++ frontend/src/0568/components/user/UserTwitterCard.jsx
@@ -0,0 +1,92 @@
+import React from "react";
+import { User, Row, Col, Text, Button, Spacer, Grid } from "@nextui-org/react";
+
+export const UserTwitterCard = () => {
+	const [following, setFollowing] = React.useState(false);
+
+	return (
+		<Grid.Container
+			className='user-twitter-card__container'
+			css={{
+				mw: "270px",
+				borderRadius: "$lg",
+				padding: "$sm",
+			}}>
+			<Row justify='space-around' align='center'>
+				<Col span={8}>
+					<User
+						src='https://i.pravatar.cc/150'
+						name='Hrithik'
+						description='@hrithikv05'
+						css={{ px: 0 }}
+					/>
+				</Col>
+				<Col span={4}>
+					<Row>
+						<Button
+							auto
+							rounded
+							onPress={() => setFollowing(!following)}
+							css={{
+								maxHeight: "$space$12",
+								fs: "$xs",
+								fontWeight: "$semibold",
+								borderColor: following
+									? "$foreground"
+									: "$primary",
+								color: following ? "$foreground" : "$white",
+							}}
+							color='primary'
+							bordered={following}>
+							{following ? "Unfollow" : "Follow"}
+						</Button>
+					</Row>
+				</Col>
+			</Row>
+			<Grid.Container className='user-twitter-card__username-container'>
+				<Grid xs={12}>
+					<Text
+						className='user-twitter-card__text'
+						size={14}
+						css={{ mt: "$1" }}
+						color='#888888'>
+						Full-stack developer 🎉
+					</Text>
+				</Grid>
+			</Grid.Container>
+
+			<Grid.Container
+				className='user-twitter-card__metrics-container'
+				justify='flex-start'
+				alignContent='center'>
+				<Text
+					className='user-twitter-card__text'
+					size={14}
+					color='#888888'>
+					<Text
+						b
+						color='foreground'
+						className='user-twitter-card__text'
+						size={14}>
+						4
+					</Text>
+					Following
+				</Text>
+				<Spacer inline x={0.5} />
+				<Text
+					className='user-twitter-card__text'
+					size={14}
+					color='#888888'>
+					<Text
+						b
+						color='foreground'
+						className='user-twitter-card__text'
+						size={14}>
+						97.1K
+					</Text>
+					Followers
+				</Text>
+			</Grid.Container>
+		</Grid.Container>
+	);
+};
