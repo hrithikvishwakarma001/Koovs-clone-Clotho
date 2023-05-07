8c670144dc6bd3dd1379cb1988cb3d0266b95f10
https://github.com/arpit10saluja/git_we.git <arpitsaluja1234@gmail.com>
2023-05-08 00:05:27 +0530

    signup done

---
+++ frontend/src/0696/pages/Cart.jsx
@@ -0,0 +1,6 @@
+
+export default function Cart() {
+  return (
+    <div>Cart</div>
+  )
+}
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,16 @@
 // import NextUIAdminPage from "./0568";
 
+import Cart from "./0696/pages/Cart";
+import Login from "./0696/pages/Login";
 import "./App.css";
+import Signup from "./0696/pages/Signup";
 
 function App() {
   return (
     <>
+    {/* <Cart/> */}
+    <Login/>
+    {/* <Signup/> */}
     </>
   );
 }
