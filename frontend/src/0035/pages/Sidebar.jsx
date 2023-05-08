ccc05a025dd87d6145458fce4b91d723afbc464b
ketan1227 <ketanbhatia27@gmail.com>
2023-05-08 23:57:28 +0530

    done

--- frontend/src/0035/pages/Sidebar.jsx
+++ frontend/src/0035/pages/Sidebar.jsx
@@ -1,6 +1,15 @@
 import { useEffect, useState } from "react";
-import { useSearchParams, } from "react-router-dom";
-import {Paginate} from "../component/Paginate"
+import { useSearchParams } from "react-router-dom";
+import { Paginate } from "../component/Paginate";
+import {
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+} from "@chakra-ui/react";
 
 export const Sidebar = () => {
   const [searchParams, setSearchParams] = useSearchParams();
@@ -11,26 +20,39 @@ export const Sidebar = () => {
   const [color, setColor] = useState(initialColor || []);
   const [order, setOrder] = useState(initialOrder || "");
   const [page, setPage] = useState(+initialPage || 1);
+  const [drawerOpen, setDrawerOpen] = useState(false);
 
   const handleChange = (e) => {
-    let newColor = [...color];
     const value = e.target.value;
+    let newColor = [...color];
+  
     if (newColor.includes(value)) {
       newColor = newColor.filter((el) => el !== value);
     } else {
       newColor.push(value);
     }
-
+  
+    // Get all the articles that match the selected colors
+    const articles = data.filter((article) => {
+      return article.swatches.some((swatch) => {
+        return newColor.includes(swatch.colorName);
+      });
+    });
+  
+    setArticles(articles);
     setColor(newColor);
   };
+  
 
   const handleSort = (e) => {
+    const currentPage = page; // store the current page number
     setOrder(e.target.value);
+    setPage(currentPage); // set the page to the current page number
   };
+  
+  
 
-  const handlePageChange = (newPage) => {
-    setPage(newPage);
-  };
+  
 
   useEffect(() => {
     let params = {
@@ -39,54 +61,74 @@ export const Sidebar = () => {
     };
     order && (params.order = order);
     setSearchParams(params);
-  }, [color, order, page]);
+  }, [color, order]);
 
   const totalPages = 7;
 
+  const handleFilterClick = () => {
+    setDrawerOpen(true);
+  };
+
+  const handleCloseDrawer = () => {
+    setDrawerOpen(false);
+  };
+
   return (
-    <>
-      <h1>Filter by</h1>
-
-      <div>
-        <input
-          type="checkbox"
-          value={"red"}
-          onChange={handleChange}
-          checked={color.includes("red")}
-        />
-        <label>Red</label>
+    <div style={{ display: "flex",margin:"50px 0px 0px 70px",gap:"15px" }}>
+      <div className="filter-button" onClick={handleFilterClick}>
+        Filter 
       </div>
+      <Drawer isOpen={drawerOpen} placement="left" onClose={handleCloseDrawer}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>Filter by</DrawerHeader>
 
-      <div>
-        <input
-          type="checkbox"
-          value={"blue"}
-          onChange={handleChange}
-          checked={color.includes("blue")}
-        />
-        <label>Blue</label>
-      </div>
+          <DrawerBody>
+            <div>
+              <input
+                type="checkbox"
+                value={"Red"}
+                onChange={handleChange}
+                checked={color.includes("Red")}
+              />
+              <label>Red</label>
+            </div>
 
-      <div>
-        <input
-          type="checkbox"
-          value={"green"}
-          onChange={handleChange}
-          checked={color.includes("green")}
-        />
-        <label>Green</label>
-      </div>
+            <div>
+              <input
+                type="checkbox"
+                value={"Blue"}
+                onChange={handleChange}
+                checked={color.includes("blue")}
+              />
+              <label>Blue</label>
+            </div>
 
-      <div>
+            <div>
+              <input
+                type="checkbox"
+                value={"Green"}
+                onChange={handleChange}
+                checked={color.includes("green")}
+              />
+              <label>Green</label>
+            </div>
+          </DrawerBody>
+
+          <DrawerFooter>
+            <button onClick={handleCloseDrawer}>Close</button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+      <div style={{ marginLeft: "10px" }}>
         <label>Sort by:</label>
         <select onChange={handleSort} value={order}>
-          <option value="">None</option>
+          <option value="">Featured</option>
           <option value="asc">Ascending</option>
           <option value="desc">Descending</option>
         </select>
       </div>
-
-     
-    </>
+    </div>
   );
 };
