ba52b2ebecac5978bdc93e38d760d3cf8d20d937
ketan1227 <ketanbhatia27@gmail.com>
2023-05-08 16:42:16 +0530

    task

--- frontend/src/0035/Redux/AuthReducer/action.js
+++ frontend/src/0035/Redux/AuthReducer/action.js
@@ -1,9 +1,14 @@
 import axios from "axios";
 import { SET_AUTH_TOKEN } from "./actionType";
+import { BASEURL } from "../../../0568/utils";
 
 
 export const getToken = (email, password) => (dispatch) => {
-  axios.post(`https://reqres.in/api/register`, { email, password }).then((res) => { dispatch({ type: SET_AUTH_TOKEN, payload: res.data }) }).then((err) => console.log(err));
+  axios.post(`${BASEURL}/register`, { email, password }).then((res) => {
+
+     dispatch({ type: SET_AUTH_TOKEN, payload: res.data })
+     
+   }).then((err) => console.log(err));
 
 
 }
